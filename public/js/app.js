413b7042c7a92bad374b1781c935d8000776e222
Kamil Sygut <k.sygut@bloomga.com>
2020-04-11 21:28:45 +0200

    59. Wiring up the User Interface

--- public/js/app.js
+++ public/js/app.js
@@ -2,19 +2,24 @@ console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', event => {
     event.preventDefault()
+
+    messageOne.textContent = 'Please wait...'
+    messageTwo.textContent = ''
 
     const location = encodeURIComponent(search.value)
     const url = `http://localhost:3333/weather?address=${location}`
 
     fetch(url).then(response => {
         response.json().then(data => {
-            if(data.error) return console.log(data.error)
+            if(data.error) return messageOne.textContent = data.error
 
-            console.log(data.location)
-            console.log(data.forecast)
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
         })
     })
 })
